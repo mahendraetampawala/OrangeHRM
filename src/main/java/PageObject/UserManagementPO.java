b289a39f6f7babf928dfd8e66c0cc8449d2c069b
@MahendraEtampawala_IT18079814 <it18079814@my.sliit.lk>
2024-07-29 23:29:14 +0530

    Initial Commit

--- src/main/java/PageObject/UserManagementPO.java
+++ src/main/java/PageObject/UserManagementPO.java
@@ -8,6 +8,14 @@ package PageObject;
  *
  */
 public class UserManagementPO {
+	
+	public static UserManagementPO UserManagement;
+	public static UserManagementPO getUserManagement() {
+		if(UserManagement==null) {
+			UserManagement=new UserManagementPO();
+		}
+		return UserManagement;
+	}
 
 	
 }
--- src/main/java/StepDefinitions/UserManagement.java
+++ src/main/java/StepDefinitions/UserManagement.java
@@ -3,10 +3,55 @@
  */
 package StepDefinitions;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 /**
  * @author mahen
  *
  */
 public class UserManagement {
+	
+	@And("user clicks on the admin")
+	public void user_clicks_on_the_admin() {
+		
+	}
+	
+	@And("verify whether the user directed to the admin user management section")
+	public void verify_whether_the_user_directed_to_the_admin_user_management_section() {
+		
+	}
 
+	@And("user enters the username as {string}")
+	public void user_enters_the_username_as(String Username){
+		
+	}
+	
+	@And("user selects the user role as {string}")
+	public void user_selects_the_user_role_as(String UserRole) {
+		
+	}
+	
+	@And("user enters the emaployee name as{string}")
+	public void user_enters_the_emaployee_name_as(String EmployeeName) {
+		
+	}
+	
+	@And("user selects the status as{string}")
+	public void user_selects_the_status_as(String Status) {
+		
+	}
+	
+	@When("user clicks on the search button")
+	public void user_clicks_on_the_search_button() {
+		
+	}
+	
+	@Then("Verify whether relavent search details are listed in the table")
+	public void Verify_whether_relavent_search_details_are_listed_in_the_table() {
+		
+	}
+	
+	
 }
